78371f750e1a5bb2b213ecc22e84d19c4072ece4
dbartoszp <barpom034@student.polsl.pl>
2023-04-21 17:31:52 +0200

    fetching from single file testing

--- js/index.js
+++ js/index.js
@@ -1,16 +1,30 @@
-const open = document.getElementById("btn-sidebar-open");
-const close = document.getElementById("btn-sidebar-close");
-const sidebar = document.getElementById("region-sidebar");
-const searchInput = document.getElementById("searchInput");
-const searchResult = document.querySelector(".npc-search-results-container");
+'use strict';
+
+const open = document.getElementById('btn-sidebar-open');
+const close = document.getElementById('btn-sidebar-close');
+const sidebar = document.getElementById('region-sidebar');
+const searchInput = document.getElementById('searchInput');
+const searchResult = document.querySelector('.npc-search-results-container');
 
 open.onclick = () => {
-  sidebar.style.left = 0;
-  open.style.display = "none";
-  close.style.display = "block";
+	sidebar.style.left = 0;
+	open.style.display = 'none';
+	close.style.display = 'block';
 };
 close.onclick = () => {
-  sidebar.style.left = "-12rem";
-  close.style.display = "none";
-  open.style.display = "block";
+	sidebar.style.left = '-12rem';
+	close.style.display = 'none';
+	open.style.display = 'block';
 };
+
+const fetchCorhyn = async () => {
+	try {
+		const response = await fetch('npc-data.json');
+		const data = await response.json();
+		console.log(data.questline[3].description);
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+fetchCorhyn();
