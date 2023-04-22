60f274d5136806c361441918b09e62cbc2778833
dbartoszp <barpom034@student.polsl.pl>
2023-04-22 15:01:27 +0200

    rewards in npc page

--- js/npc-page-model.js
+++ js/npc-page-model.js
@@ -9,6 +9,7 @@ const npcContainer = document.getElementById('npc-info-container');
 const table = document.getElementById('table-of-contents');
 const generalInfo = document.getElementById('npc-info-general');
 const mainInfo = document.getElementById('npc-info-main');
+const rewards = document.getElementById('rewards');
 
 let markup = '';
 
@@ -66,43 +67,60 @@ const generateMarkup = (npc, where) => {
 				// 	ound later in: location2, location3<br /> -->
 			}
 			break;
-		case 'main': {
-			let tmp;
-			markup = `<h2>${npc.mainName}'s questline step by step</h2>`;
-			npc.questline.forEach((el, i) => {
-				markup += `<h3 id="${el.step}"><span>${i + 1}. ${
-					el.step
-				}</span></br> ${el.description}
+		case 'main':
+			{
+				let tmp;
+				markup = `<h2>${npc.mainName}'s questline step by step</h2>`;
+				npc.questline.forEach((el, i) => {
+					markup += `<h3 id="${el.step}"><span>${i + 1}. ${
+						el.step
+					}</span></br> ${el.description}
                 ${
 					el.mapLink
 						? `</br><a href="${el.mapLink}" target="_blank" rel="noopener noreferrer">[Fextralife map link]</a></h3>`
 						: ``
 				}
                     `;
-			});
-			// <h2>npcs questline step by step</h2>
-			// 		<h3>
-			// 			1. Lorem ipsum dolor sit amet, consectetur adipiscing
-			// 			elit. Morbi facilisis, eros id tristique tristique,
-			// 			ligula lacus congue risus, eu gravida metus libero in
-			// 			mauris. Mauris ullamcorper metus dui, at egestas odio
-			// 			euismod ut. Phasellus lacinia ac erat vel consequat.
-			// 			Integer auctor nec dui id fringilla. Suspendisse aliquam
-			// 			aliquam erat eu blandit. Nulla facilisi. Nulla facilisi.
-			// 			Sed efficitur dui sed magna convallis accumsan. Donec
-			// 			aliquet, odio nec efficitur finibus, orci mi euismod
-			// 			lorem, non varius neque erat in sem. Morbi turpis metus,
-			// 			sodales at tempor at, venenatis nec leo. Morbi mattis
-			// 			diam hendrerit ligula hendrerit fringilla. Integer purus
-			// 			ligula, vestibulum in porttitor nec, consequat in quam.
-			// 			Nunc posuere dolor in purus tincidunt posuere.
-			// 		</h3>
-			// 		<img src="/vendors/img/bgs/caelid-background.png" />
-			// 		<h3>
-			// 			2. Mauris dui lacus, consequat sed vehicula a, pharetra
-			// 			sed ligula. Cras suscipit sem id hendrerit cursus. Sed
-			// 			cursus malesuada purus vel aliquam. Nullam mollis
-		}
+				});
+				// <h2>npcs questline step by step</h2>
+				// 		<h3>
+				// 			1. Lorem ipsum dolor sit amet, consectetur adipiscing
+				// 			elit. Morbi facilisis, eros id tristique tristique,
+				// 			ligula lacus congue risus, eu gravida metus libero in
+				// 			mauris. Mauris ullamcorper metus dui, at egestas odio
+				// 			euismod ut. Phasellus lacinia ac erat vel consequat.
+				// 			Integer auctor nec dui id fringilla. Suspendisse aliquam
+				// 			aliquam erat eu blandit. Nulla facilisi. Nulla facilisi.
+				// 			Sed efficitur dui sed magna convallis accumsan. Donec
+				// 			aliquet, odio nec efficitur finibus, orci mi euismod
+				// 			lorem, non varius neque erat in sem. Morbi turpis metus,
+				// 			sodales at tempor at, venenatis nec leo. Morbi mattis
+				// 			diam hendrerit ligula hendrerit fringilla. Integer purus
+				// 			ligula, vestibulum in porttitor nec, consequat in quam.
+				// 			Nunc posuere dolor in purus tincidunt posuere.
+				// 		</h3>
+				// 		<img src="/vendors/img/bgs/caelid-background.png" />
+				// 		<h3>
+				// 			2. Mauris dui lacus, consequat sed vehicula a, pharetra
+				// 			sed ligula. Cras suscipit sem id hendrerit cursus. Sed
+				// 			cursus malesuada purus vel aliquam. Nullam mollis
+			}
+			break;
+		case 'rewards':
+			{
+				markup = `<h2>Rewards</h2><h3>`;
+				npc.rewards.forEach((el, i) => {
+					markup += '- ' + el + '</br>';
+				});
+
+				// <h2>Rewards</h2>
+				// 				<h3>
+				// 					2. steps to complete questl<br />
+				// 					3. steps to complete quest<br />
+				// 					4. steps to complete quest<br />
+				// 				</h3>
+			}
+			break;
 	}
 	return markup;
 };
@@ -113,6 +131,7 @@ const titleMarkup = generateMarkup(npcFetched, 'title');
 const tableMarkup = generateMarkup(npcFetched, 'table');
 const generalMarkup = generateMarkup(npcFetched, 'general');
 const mainMarkup = generateMarkup(npcFetched, 'main');
+const rewardsMarkup = generateMarkup(npcFetched, 'rewards');
 
 const insertMarkup = (markup, where) => {
 	where.insertAdjacentHTML('afterbegin', markup);
@@ -122,3 +141,4 @@ insertMarkup(titleMarkup, npcContainer);
 insertMarkup(tableMarkup, table);
 insertMarkup(generalMarkup, generalInfo);
 insertMarkup(mainMarkup, mainInfo);
+insertMarkup(rewardsMarkup, rewards);
