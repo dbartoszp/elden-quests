3c44c78daef4a266b5709bde5dd5eb6b249a64c7
dbartoszp <dbartosz.pomierny@wp.pl>
2023-04-25 20:31:29 +0200

    region data template for dynamic search start

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
-'use strict';
-import * as searchbar from './searchbar.js';
+"use strict";
+import * as searchbar from "./searchbar.js";
 
-const open = document.getElementById('btn-sidebar-open');
-const close = document.getElementById('btn-sidebar-close');
-const sidebar = document.getElementById('region-sidebar');
-const searchInput = document.getElementById('search-input');
-const results = document.getElementById('npc-results');
+const open = document.getElementById("btn-sidebar-open");
+const close = document.getElementById("btn-sidebar-close");
+const sidebar = document.getElementById("region-sidebar");
+const searchInput = document.getElementById("search-input");
+const results = document.getElementById("npc-results");
 const resultsContainer = document.getElementById(
-	'npc-search-results-container'
+  "npc-search-results-container"
 );
+const npcTemplate = document.querySelector("[npc-template]");
 
 open.onclick = () => {
-	sidebar.style.left = 0;
-	open.style.display = 'none';
-	close.style.display = 'block';
+  sidebar.style.left = 0;
+  open.style.display = "none";
+  close.style.display = "block";
 };
 close.onclick = () => {
-	sidebar.style.left = '-12rem';
-	close.style.display = 'none';
-	open.style.display = 'block';
+  sidebar.style.left = "-12rem";
+  close.style.display = "none";
+  open.style.display = "block";
 };
