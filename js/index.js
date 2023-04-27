f5875a215d5a50830ee017fd8f73b30e0f28354d
dbartoszp <dbartosz.pomierny@wp.pl>
2023-04-28 00:57:20 +0200

    dynamic search TO BE REFACTORED

--- js/index.js
+++ js/index.js
@@ -5,12 +5,30 @@ const open = document.getElementById("btn-sidebar-open");
 const close = document.getElementById("btn-sidebar-close");
 const sidebar = document.getElementById("region-sidebar");
 const searchInput = document.getElementById("search-input");
-const results = document.getElementById("npc-results");
-const resultsContainer = document.getElementById(
-  "npc-search-results-container"
-);
+
 const npcsTableContainer = document.getElementById("npcs-table-container");
 const npcTemplate = document.querySelector("[npc-template]");
+
+let allNpcs = await searchbar.fetchNpcs();
+let availableNpcs = await searchbar.fetchNpcs();
+let allRegionNpcs = await searchbar.fetchRegions();
+allRegionNpcs.forEach((e) => console.log(e.npcs));
+const availableNpcsArray = availableNpcs.map((el) => el.id);
+const allNpcsArray = allRegionNpcs.map((el) => el.npcs);
+console.log(allNpcsArray);
+console.log(availableNpcsArray);
+
+// const mergedArray =
+
+searchInput.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
+  const filteredNpcs = allNpcs.filter((npc) =>
+    npc.mainName.toLowerCase().includes(searchString)
+  );
+  console.log("filtered:", filteredNpcs);
+  displayNpcs(filteredNpcs);
+  // console.log(filteredNpcs);
+});
 
 open.onclick = () => {
   sidebar.style.left = 0;
@@ -23,33 +41,29 @@ close.onclick = () => {
   open.style.display = "block";
 };
 
-export const displayNpcs = async () => {
+const displayNpcs = async (npcsParam) => {
+  console.log("param:", npcsParam);
   const regionsArr = await searchbar.fetchRegions();
   const npcsArr = await searchbar.fetchNpcs();
+  // console.log(matching);
   // console.log(npcsArr);
   // console.log(getExactNpc(npcsArr, "fia"));
-  // console.log(regionsArr);
+  console.log(regionsArr);
   let htmlString = ``;
   let exactNpc = ``;
-  let matchingNpcsArr;
   let currentNpc;
-  regionsArr.forEach((region) => {
+  const givenNpcs = npcsParam.map((npc) => npc.id);
+  console.log("given:", givenNpcs);
+  regionsArr.forEach((region, index) => {
     let columnCount = 0;
     let rowCount = 0;
     let npcCount = 0;
-    matchingNpcsArr = region.npcs.filter((npc) => {
-      if (
-        npcsArr.some((el) => {
-          return el.id === npc;
-        })
-      )
-        return npc;
+    const merged = allNpcsArray[index].filter((npc) => {
+      return givenNpcs.includes(npc);
     });
-    // console.log(region.npcs);
-    // console.log(matchingNpcsArr);
     if (
       region.npcs.some((el) => {
-        return matchingNpcsArr.includes(el);
+        return merged.includes(el);
       })
     ) {
       htmlString += `<div class="npcs-table-region-name" id="region-${region.id}">
@@ -58,19 +72,17 @@ export const displayNpcs = async () => {
         htmlString += `<div class="row">`;
         rowCount += 1;
       }
-      matchingNpcsArr.forEach((npc) => {
-        currentNpc = searchbar.getExactNpc(npcsArr, matchingNpcsArr[npcCount]);
+      merged.forEach((npc) => {
+        currentNpc = searchbar.getExactNpc(npcsArr, merged[npcCount]);
         htmlString += `<div class="column-1-of-3">
           <div class="npc-container">
             <a class="img-link" href="npc-page.html?npc=${
-              matchingNpcsArr[npcCount]
+              merged[npcCount]
             }"><img src="${
-          searchbar.getExactNpc(npcsArr, matchingNpcsArr[npcCount]).mainImage
+          searchbar.getExactNpc(npcsArr, merged[npcCount]).mainImage
         }" /></a>
-            <a class="text-link" href="npc-page.html?npc=${
-              matchingNpcsArr[npcCount]
-            }">${
-          searchbar.getExactNpc(npcsArr, matchingNpcsArr[npcCount]).mainName
+            <a class="text-link" href="npc-page.html?npc=${merged[npcCount]}">${
+          searchbar.getExactNpc(npcsArr, merged[npcCount]).mainName
         }</a>
           </div>
         </div>`;
@@ -82,7 +94,7 @@ export const displayNpcs = async () => {
       htmlString += `</div>`;
     }
   });
-  console.log(htmlString);
+  // console.log(htmlString);
   npcsTableContainer.innerHTML = htmlString;
 };
 
