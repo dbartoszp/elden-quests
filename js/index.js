254ee96b5e1796fe36f5b5336c89a336a4e61574
dbartoszp <dbartosz.pomierny@wp.pl>
2023-04-28 01:07:32 +0200

    little bit of cleaning up + test data

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 "use strict";
-import * as searchbar from "./searchbar.js";
+import { fetchNpcs, fetchRegions, getExactNpc } from "./searchbar.js";
 
 const open = document.getElementById("btn-sidebar-open");
 const close = document.getElementById("btn-sidebar-close");
@@ -7,25 +7,23 @@ const sidebar = document.getElementById("region-sidebar");
 const searchInput = document.getElementById("search-input");
 
 const npcsTableContainer = document.getElementById("npcs-table-container");
-const npcTemplate = document.querySelector("[npc-template]");
 
-let allNpcs = await searchbar.fetchNpcs();
-let availableNpcs = await searchbar.fetchNpcs();
-let allRegionNpcs = await searchbar.fetchRegions();
+let availableNpcs = await fetchNpcs();
+let allRegionNpcs = await fetchRegions();
 allRegionNpcs.forEach((e) => console.log(e.npcs));
 const availableNpcsArray = availableNpcs.map((el) => el.id);
 const allNpcsArray = allRegionNpcs.map((el) => el.npcs);
-console.log(allNpcsArray);
-console.log(availableNpcsArray);
+// console.log(allNpcsArray);
+// console.log(availableNpcsArray);
 
 // const mergedArray =
 
 searchInput.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
-  const filteredNpcs = allNpcs.filter((npc) =>
+  const filteredNpcs = availableNpcs.filter((npc) =>
     npc.mainName.toLowerCase().includes(searchString)
   );
-  console.log("filtered:", filteredNpcs);
+  // console.log("filtered:", filteredNpcs);
   displayNpcs(filteredNpcs);
   // console.log(filteredNpcs);
 });
@@ -42,18 +40,18 @@ close.onclick = () => {
 };
 
 const displayNpcs = async (npcsParam) => {
-  console.log("param:", npcsParam);
-  const regionsArr = await searchbar.fetchRegions();
-  const npcsArr = await searchbar.fetchNpcs();
+  // console.log("param:", npcsParam);
+  const regionsArr = await fetchRegions();
+  const npcsArr = await fetchNpcs();
   // console.log(matching);
   // console.log(npcsArr);
   // console.log(getExactNpc(npcsArr, "fia"));
-  console.log(regionsArr);
+  // console.log(regionsArr);
   let htmlString = ``;
   let exactNpc = ``;
   let currentNpc;
   const givenNpcs = npcsParam.map((npc) => npc.id);
-  console.log("given:", givenNpcs);
+  // console.log("given:", givenNpcs);
   regionsArr.forEach((region, index) => {
     let columnCount = 0;
     let rowCount = 0;
@@ -73,16 +71,16 @@ const displayNpcs = async (npcsParam) => {
         rowCount += 1;
       }
       merged.forEach((npc) => {
-        currentNpc = searchbar.getExactNpc(npcsArr, merged[npcCount]);
+        currentNpc = getExactNpc(npcsArr, merged[npcCount]);
         htmlString += `<div class="column-1-of-3">
           <div class="npc-container">
             <a class="img-link" href="npc-page.html?npc=${
               merged[npcCount]
             }"><img src="${
-          searchbar.getExactNpc(npcsArr, merged[npcCount]).mainImage
+          getExactNpc(npcsArr, merged[npcCount]).mainImage
         }" /></a>
             <a class="text-link" href="npc-page.html?npc=${merged[npcCount]}">${
-          searchbar.getExactNpc(npcsArr, merged[npcCount]).mainName
+          getExactNpc(npcsArr, merged[npcCount]).mainName
         }</a>
           </div>
         </div>`;
